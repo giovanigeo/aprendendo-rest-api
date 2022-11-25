b1bc0ec52ae755055121014e29e62d0e0f3e4f46
Giovani Moreira <giovani_moreira220@outlook.com>
2022-11-25 11:09:14 -0300

    criado o repository

---
+++ src/main/java/com/lojaapi/model/ProdutoModel.java
@@ -0,0 +1,43 @@
+package com.lojaapi.model;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_produtos")
+public class ProdutoModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private UUID id;
+	private String nome;
+	private BigDecimal preco;
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public BigDecimal getPreco() {
+		return preco;
+	}
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/com/lojaapi/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.lojaapi.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lojaapi.model.ProdutoModel;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<ProdutoModel, UUID> {
+
+}
