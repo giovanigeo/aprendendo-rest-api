738032b03d6136d8034c89576cda50d6f3b06555
Giovani Moreira <giovani_moreira220@outlook.com>
2022-11-25 10:52:31 -0300

    primeiro commit

---
+++ src/main/java/com/lojaapi/LojaTesteApplication.java
@@ -0,0 +1,13 @@
+package com.lojaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaTesteApplication.class, args);
+	}
+
+}
