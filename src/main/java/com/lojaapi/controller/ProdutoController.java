ba6789e07426929b292b6a981ff3a210d334ae22
Giovani Moreira <giovani_moreira220@outlook.com>
2022-11-25 11:54:18 -0300

    implementação dos métodos POST, DELETE e PUT

--- src/main/java/com/lojaapi/controller/ProdutoController.java
+++ src/main/java/com/lojaapi/controller/ProdutoController.java
@@ -7,8 +7,12 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lojaapi.model.ProdutoModel;
@@ -26,11 +30,37 @@ public class ProdutoController {
 	}
 	
 	@GetMapping("/produtos/{id}")
-	public ResponseEntity<ProdutoModel> getOneProduto(@PathVariable(value = id) UUID id){
+	public ResponseEntity<ProdutoModel> getOneProduto(@PathVariable(value = "id") UUID id){
 		Optional<ProdutoModel> produtoOp = repository.findById(id);
 		if(produtoOp.isEmpty()) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		return new ResponseEntity<ProdutoModel>(produtoOp.get(), HttpStatus.OK);
 	}
+	
+	@PostMapping("/produtos")
+	public ResponseEntity<ProdutoModel> saveProduto(@RequestBody @Validated ProdutoModel produto){
+		return new ResponseEntity<ProdutoModel>(repository.save(produto), HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping("/produto/{id}")
+	public ResponseEntity<ProdutoModel> deleteProduto (@PathVariable(value = "id") UUID id){
+		Optional<ProdutoModel> produtoOp = repository.findById(id);
+		if(produtoOp.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		repository.delete(produtoOp.get());
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	public ResponseEntity<ProdutoModel> updateProduto (@PathVariable(value = "id") UUID id,  @RequestBody @Validated ProdutoModel produto){
+		Optional<ProdutoModel> produtoOp = repository.findById(id);
+		if(produtoOp.isEmpty()) {
+			new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		produto.setId(produtoOp.get().getId());
+		return new ResponseEntity<ProdutoModel>(repository.save(produto), HttpStatus.OK);
+	}
+	
+	
 }
