2a410110f0cb04b718bd0d9e6ace1d824df034eb
Giovani Moreira <giovani_moreira220@outlook.com>
2022-11-25 11:57:02 -0300

    correção dos métodos

--- src/main/java/com/lojaapi/controller/ProdutoController.java
+++ src/main/java/com/lojaapi/controller/ProdutoController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -53,6 +54,7 @@ public class ProdutoController {
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
+	@PutMapping("/produtos/{id}")
 	public ResponseEntity<ProdutoModel> updateProduto (@PathVariable(value = "id") UUID id,  @RequestBody @Validated ProdutoModel produto){
 		Optional<ProdutoModel> produtoOp = repository.findById(id);
 		if(produtoOp.isEmpty()) {
